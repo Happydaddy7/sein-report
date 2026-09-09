58b70b96c459170245d5ed805b7dc52b53ffd4be
Happydaddy7 <lbj.sein@gmail.com>
2026-09-09 11:48:35 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+// 슬랙 플러스(+) PWA 서비스워커
+// 버전 올릴 때 CACHE 이름을 변경하면 이전 캐시가 정리됨
+const CACHE = 'sein-report-v40';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// network-first: 항상 최신을 우선 시도하고, 실패 시 캐시로 폴백
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then((r) => r || caches.match('./index.html')))
+  );
+});
